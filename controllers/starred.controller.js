61beccf37b1b7f00927b24ac39a8ad9504d1a6f7
Muhammad Talha <muhammadtalha517@gmail.com>
2023-12-29 10:22:38 +0500

    Add pagination and update functionality to starred controller

--- controllers/starred.controller.js
+++ controllers/starred.controller.js
@@ -1,11 +1,32 @@
 import Starred from "../models/starred.model.js";
 
 export const getStarred = async (req, res) => {
-  Starred.findAll().then((starred) => {
-    res
-      .status(200)
-      .json({ data: starred, message: "Starred list fetched successfully" });
-  });
+  // pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const results = { pagination: {} };
+  const total = await Starred.count();
+  if (endIndex < total) {
+    results.pagination.next = page + 1;
+  }
+  if (startIndex > 0) {
+    results.pagination.previous = page - 1;
+  }
+  results.pagination.limit = limit;
+  results.pagination.totalPages = Math.ceil(total / limit);
+  results.pagination.currentPage = page;
+
+  try {
+    results.data = await Starred.findAll({
+      offset: startIndex,
+      limit: limit,
+    });
+    res.json(results);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 };
 
 export const createStarred = async (req, res) => {
@@ -23,5 +44,16 @@ export const createStarred = async (req, res) => {
 export const deleteStarred = async (req, res) => {
   Starred.destroy({ where: { id: req.body.id } }).then((starred) => {
     res.status(200).json({ message: "Starred deleted successfully" });
+  });
+};
+
+export const updateStarred = async (req, res) => {
+  if (!req.params.id)
+    return res.status(400).json({ error: "Please provide id" });
+  Starred.update(
+    { product_rating: req.body.product_rating },
+    { where: { id: req.params.id } }
+  ).then((starred) => {
+    res.status(200).json({ message: "Starred updated successfully" });
   });
 };
--- routes/starred.routes.js
+++ routes/starred.routes.js
@@ -3,12 +3,13 @@ import {
   createStarred,
   deleteStarred,
   getStarred,
+  updateStarred,
 } from "../controllers/starred.controller.js";
 
 const router = Router();
 
 router.route("/").get(getStarred).post(createStarred);
 
-router.route("/:id").delete(deleteStarred);
+router.route("/:id").patch(updateStarred).delete(deleteStarred);
 
 export default router;

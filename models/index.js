3e53602f2553f1f56c0d70f437dee7ac15accfc7
Muhammad Talha <muhammadtalha517@gmail.com>
2023-12-29 10:40:16 +0500

    Refactor database configuration in models/index.js and added sample env

--- models/index.js
+++ models/index.js
@@ -2,21 +2,25 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import { Sequelize } from "sequelize";
 
+const options = Boolean(process.env.LOCALDATABASE)
+  ? {}
+  : {
+      dialectOptions: {
+        ssl: {
+          rejectUnauthorized: true,
+        },
+      },
+      define: {
+        timestamps: false,
+      },
+    };
 const sequelize = new Sequelize({
   dialect: "mysql", // or the dialect for your database
   database: process.env.DATABASE,
   username: process.env.DB_USERNAME,
   host: process.env.DB_HOST,
   password: process.env.DB_PASSWORD,
-
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  },
-  define: {
-    timestamps: false,
-  },
+  ...options,
   logging: false, // disable logging SQL queries to the console
 });
 
